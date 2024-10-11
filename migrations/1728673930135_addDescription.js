533fdb0814784c15df985f21c61367305538ccb6
Elizaveta Kotelnikova <liza1408@icloud.com>
2024-10-11 22:32:22 +0300

    feat: second migration

---
+++ migrations/1728673930135_addDescription.js
@@ -0,0 +1,11 @@
+exports.up = (pgm) => {
+    pgm.addColumns('clubs', {
+        description : { type: 'text', notNull: false},
+    });
+};
+
+exports.down = (pgm) => {
+    pgm.dropColumns('clubs', {
+        description : { type: 'text', notNull: false},
+    });
+};
