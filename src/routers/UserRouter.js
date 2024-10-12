4e6b22562121b64d73fd8eac2b5fc8fc1dbb7d45
Elizaveta Kotelnikova <liza1408@icloud.com>
2024-10-12 12:39:31 +0300

    feat: user router implemented

--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -1,11 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const crypto = require("crypto");
-const jwt = require("jsonwebtoken")
-const jwtSecret = "3cc9671ea78296449b91ce1e02c7b0fd5c32411c7cb5029f2117bfc8754753e9a75ec6"
+const UserModel = require('../models/UserModel')
 const usersService = require('../services/UserService');
 
-router.get("/api/signup", (req, res, next) => {
+router.post("/api/signup", (req, res) => {
     let user = new UserModel(crypto.randomUUID(), req.isu, null, null, null)
     try {
         usersService.createUser(user);
@@ -19,4 +18,55 @@ router.get("/api/signup", (req, res, next) => {
     // view?????????? мне же надо только json возвращать
     //res.render("about_view", { title: "About", list: queryResults });
     res.render(JSON.stringify(user));
+});
+
+router.get("/api/user/:userId", (req, res) => {
+    let user = null
+    try {
+        user = usersService.getUserById(req.params.userId);
+    }
+    catch (e) {
+        return res
+            .status(400)
+            .json({ message: "Bad request" })
+    }
+
+    if (user == null) {
+        return res
+            .status(404)
+            .json({message: "Not found"})
+    }
+
+    res.render(JSON.stringify(user));
+});
+
+router.put("/api/user/", (req, res) => {
+    let user = new UserModel(crypto.randomUUID(), req.isu, req.firstName, req.lastName, req.course)
+    try {
+        usersService.updateUser(user);
+    }
+    catch (e) {
+        return res
+            .status(400)
+            .json({ message: "Bad request" })
+    }
+
+    if (user == null) {
+        return res
+            .status(404)
+            .json({message: "Not found"})
+    }
+
+    res.render(JSON.stringify(user));
+});
+
+router.delete("/api/user/:userId", (req, res) => {
+    try {
+        usersService.deleteUser(req.params.userId);
+    }
+    catch (e) {
+        return res
+            .status(500)
+            .json({ message: "Internal server error" })
+    }
 });
